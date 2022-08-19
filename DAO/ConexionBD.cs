cd630df61caccd456b0ce3e453020f076d9ccd56
Karla Santa Cruz <kvane2816@hotmail.com>
2022-08-18 23:25:11 -0500

    .....

--- DAO/ConexionBD.cs
+++ DAO/ConexionBD.cs
@@ -14,9 +14,9 @@ namespace DAO
             get
             {
                 //ale
-                return @"data source=ALEPC; initial catalog=BD_SGIAMTv1; integrated security=SSPI;";
+                //return @"data source=ALEPC; initial catalog=BD_SGIAMTv1; integrated security=SSPI;";
                 //karla
-                //return @"data source=DESKTOP-NEB97S6; initial catalog=BD_SGIAMTv1; integrated security=SSPI;";
+                return @"data source=DESKTOP-NEB97S6; initial catalog=BD_SGIAMTv1; integrated security=SSPI;";
             }
         }
     }
